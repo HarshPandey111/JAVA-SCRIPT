3ccbd2aff63d467ab7184e112729fcc4c3fd6bbb
HarshPandey111 <harshpandey70523@gmail.com>
2025-03-20 17:15:19 +0530

    hello

---
+++ day5/array.js
@@ -0,0 +1,23 @@
+const ar=[1,2,3,4,5]
+console.log(ar[0])
+
+// ar[1]=22
+
+// console.log(ar[1])
+
+//array methods
+ar.push(23)
+ar.push(24)
+ar.push(25)
+// console.log( ar.length); 
+ar.unshift(0)
+ar.shift()//right shift
+console.log(ar.includes(36));
+console.log(ar.indexOf(4));
+const newarr=ar.join();
+console.log(typeof newarr);
+
+//slice
+//splice remove that portion of element
+console.log(ar);
+
